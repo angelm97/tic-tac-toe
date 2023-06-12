677af1d11190230d0d21e1a7ffd64e8592dfb270
angel <angelmeregildo@live.com>
2023-06-12 19:52:16 -0400

    bot level 2

--- src/components/BotGamer.jsx
+++ src/components/BotGamer.jsx
@@ -4,15 +4,42 @@ import React from 'react'
 
 
 const BotGamer = (turn, TURNS, board, setBoard, checkWinnerFrom, checkEndGame, setTurn, saveGameToStorage, setWinner, mode) => {
-
+    let play = false;
     let exit = true;
+    const WINNER_COMBOS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
     if (turn == TURNS.X && mode == '/bot') {
+        const newBoard = [...board]
+        for (const combo of WINNER_COMBOS) {
+            const [a, b, c] = combo
+            if (newBoard[a] && newBoard[a] === newBoard[b]) {
+                play = c
+            } else if (newBoard[a] && newBoard[a] === newBoard[c]) {
+                play = b
+            }
+        }
+
         while (exit) {
             const randomnumber = Math.floor((Math.random() * 9))
             if (board[randomnumber] === null) {
-                const newBoard = [...board]
-                newBoard[randomnumber] = turn
+
+
+
+                if (play != false && board[play] === null) {
+                    newBoard[play] = turn
+                } else {
+                    newBoard[randomnumber] = turn
+                }
+
                 setBoard(newBoard)
 
                 const newWinner = checkWinnerFrom(newBoard)
@@ -30,12 +57,15 @@ const BotGamer = (turn, TURNS, board, setBoard, checkWinnerFrom, checkEndGame, s
                     board: newBoard,
                     turn: newTurn
                 })
+
+
                 exit = (false)
             }
 
             if (board.every((square) => square !== null)) {
                 exit = (false)
             }
+
 
         }
     }
